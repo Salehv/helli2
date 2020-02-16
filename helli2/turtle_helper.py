13f100f46c68bc9b59ed21fa7f9cfbd1712672ed
Saleh <salehv@gmail.com>
2020-02-16 21:57:33 +0330

    minor fix

--- helli2/turtle_helper.py
+++ helli2/turtle_helper.py
@@ -5,9 +5,9 @@ def islamic_draw(repeat, rotation, pencolor, fillcolor, function, *args):
     t.home()
     t.color(pencolor, fillcolor)
     for i in range(repeat):
-        begin_fill()
+        t.begin_fill()
         function(*args)
-        end_fill()
+        t.end_fill()
         t.left(rotation)
     t.color("black", "black")
     t.home()
