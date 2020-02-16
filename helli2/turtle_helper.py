a2408934c919d91b842abb27dafe65bb8435f01e
Saleh <salehv@gmail.com>
2020-02-16 21:51:15 +0330

    fixed color problem

--- helli2/turtle_helper.py
+++ helli2/turtle_helper.py
@@ -1,13 +1,15 @@
 import turtle as t
 
-def islamic_draw(rotation, repeat, pencolor, fillcolor, function, *args):
+def islamic_draw(repeat, rotation, pencolor, fillcolor, function, *args):
     t.speed(-1)
     t.home()
     t.color(pencolor, fillcolor)
     for i in range(repeat):
-        function(*args)        
+        begin_fill()
+        function(*args)
+        end_fill()
         t.left(rotation)
-    t.color("black, "black")
+    t.color("black", "black")
     t.home()
 
 
