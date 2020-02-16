11440c975b88911484eba457bc13bf28afed0bbb
Saleh <salehv@gmail.com>
2020-02-16 21:23:59 +0330

    download source changed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Saleh v',                   # Type in your name
   author_email = 'salehv@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Salehv/helli2',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Salehv/helli2/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Salehv/helli2/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['helli2', 'school work'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
